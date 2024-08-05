9d61dedcf2b3e055cb789070491c1a8f902e04e2
SahityaSingh01 <sahitya.singh@kissht.com>
2024-08-05 23:53:11 +0530

    Added Delete operation in Go

--- main.go
+++ main.go
@@ -28,10 +28,19 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
-// func deleteMovie(w http.ResponseWriter, r *http.Request){
-// 	w.Header().Set("Content-Type","application/json")
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
 
-// }
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+
+}
 
 func main() {
 	r := mux.NewRouter()
@@ -43,7 +52,7 @@ func main() {
 	// r.HandleFunc("/movies/{id}",getMovie).Methods("GET")
 	// r.HandleFunc("/movies",createMovie).Methods("POST")
 	// r.HandleFunc("/movies/{id}",updateMovie).Methods("PUT")
-	// r.HandleFunc("/movies/{id}",deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Printf("starting server at 8000\n")
 	log.Fatal(http.ListenAndServe(":8000", r))
